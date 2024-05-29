d643bb8a9785e25d7d1bf358bc267aede1994098
Michael Wawra <michael@wawra.co.uk>
2024-05-29 19:43:59 +0100

    Display neatness...

--- src/main.c
+++ src/main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "pd_api.h"
 
 static int update(void* userdata);
+void ProcessInput(PlaydateAPI* pd);
+void drawMaze(PlaydateAPI* pd);
+LCDBitmap* getFrame(PlaydateAPI *pd, int x, int y);
+void init(PlaydateAPI *pd);
+void build_maze();
+
+const int MAZE_X = 12;
+const int MAZE_Y = 7;
+const int MAZE_LINEAR = 84;
+const int X_OFFSET = 7;
+const int Y_OFFSET = 8;
+
+struct {
+  int SCREEN_WIDTH;
+  int SCREEN_HEIGHT;
+  float frame;
+  LCDBitmapTable* bitmapTable;
+  const char* err;
+	int maze[MAZE_LINEAR];
+} GAMEDATA;
+
+
 const char* fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
 LCDFont* font = NULL;
 
 #ifdef _WINDLL
 __declspec(dllexport)
 #endif
+
 int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 {
 	(void)arg; // arg is currently only used for event = kEventKeyPressed
@@ -31,65 +55,28 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 	return 0;
 }
 
-
-#define TEXT_WIDTH 86
-#define TEXT_HEIGHT 16
-
-int x = (400-TEXT_WIDTH)/2;
-int y = (240-TEXT_HEIGHT)/2;
-int dx = 1;
-int dy = 2;
-char buttonState[100];
-
-
-struct {
-  int SCREEN_WIDTH;
-  int SCREEN_HEIGHT;
-  float frame;
-  float frameMax;
-  LCDBitmapTable* bitmapTable;
-  const char* err;
-} GAMEDATA;
-
-void ProcessInput(PlaydateAPI* pd);
-void drawMaze(PlaydateAPI* pd);
-LCDBitmap* getFrame(PlaydateAPI *pd);
-
 void init(PlaydateAPI *pd)
 {  
-	// LCDBitmapTable* playdate->graphics->loadBitmapTable(const char* path, const char** outerr);
+  const char* wall_table = "walls"; // "robot-table-8-8.png";
   GAMEDATA.bitmapTable = pd->graphics->loadBitmapTable( wall_table, &GAMEDATA.err );
 
 	if (GAMEDATA.bitmapTable == NULL)
 	{
     pd->system->error( "Could not load bitmap table: %s", GAMEDATA.err );
 	}
-	else pd->system->logToConsole( "Got bitmap");
+	else pd->system->logToConsole( "Got bitmap, ready to roll.");
+
+	build_maze();
+
+	srand(time(NULL));
 }
 
 static int update(void* userdata)
 {
 	PlaydateAPI* pd = userdata;
-	
 	pd->graphics->clear(kColorWhite);
-	pd->graphics->setFont(font);
-
   ProcessInput(pd);
-
-	pd->graphics->drawText(buttonState, strlen(buttonState), kASCIIEncoding, x, y);
-
-	// x += dx;
-	// y += dy;
-	
-	// if ( x < 0 || x > LCD_COLUMNS - TEXT_WIDTH )
-	// 	dx = -dx;
-	
-	// if ( y < 0 || y > LCD_ROWS - TEXT_HEIGHT )
-	// 	dy = -dy;
-
   drawMaze(pd);
-
-	// pd->system->drawFPS(0,0);
 
 	return 1;
 }
@@ -105,57 +92,37 @@ void ProcessInput(PlaydateAPI* pd)
   // else if ( pushed & kButtonUp    ) { GAMEDATA.frame++;     }
   // else if ( pushed & kButtonDown  ) { GAMEDATA.frame--;   }
   // else if ( pushed & kButtonRight ) { GAMEDATA.frame++;  }
-
-  // if ( current & kButtonLeft  ) { strcpy( buttonState, "left held" );   }
-  // else if ( current & kButtonUp    ) { strcpy( buttonState, "up held" );     }
-  // else if ( current & kButtonDown  ) { strcpy( buttonState, "down held" );   }
-  // else if ( current & kButtonRight ) { strcpy( buttonState, "right held" );  }
-  // else if ( current & kButtonA     ) { strcpy( buttonState, "a held" );      }
-  // else if ( current & kButtonB     ) { strcpy( buttonState, "b held" );      }
-  // else if ( released ) { strcpy( buttonState, "" );   }
 }
-
 
 void drawMaze(PlaydateAPI* pd)
 {
-//  enum LCDBitmapColor white = kColorWhite;
-	//pd->graphics->clear(kColorWhite);
-
-
-  // Load a bitmap table
-  const char* wall_table = "walls"; // "robot-table-8-8.png";
-
-
- 
-
-	// char *frame;
-	// sprintf(frame, "%1.0f", GAMEDATA.frame );
-  // strcpy( buttonState, frame); 
-
-
+	//Loop over the grid like it's 1992...
 	int x = 0;
 	int y = 0;
 
+	// Until we can create a maze, just loop through different aspects.
   GAMEDATA.frame = 0;
 
-	for (x=0;x<20;x++)
+	for (x=0;x<MAZE_X;x++)
 	{
-		for (y=0;y<20;y++)
+		for (y=0;y<MAZE_Y;y++)
 		{
-			 
-       pd->graphics->drawBitmap( getFrame(pd), 10+(x*8), 10+(y*8), 0 );
+       pd->graphics->drawScaledBitmap( getFrame(pd, x, y), X_OFFSET+(x*32), Y_OFFSET+(y*32), 4, 4 );
 		}
 	}
 }
 
 
-LCDBitmap* getFrame(PlaydateAPI *pd)
+LCDBitmap* getFrame(PlaydateAPI *pd, int x, int y)
 {
-  LCDBitmap* currentFrame = pd->graphics->getTableBitmap( GAMEDATA.bitmapTable, GAMEDATA.frame );
-	GAMEDATA.frame++;
-	if ( GAMEDATA.frame >= 14 || GAMEDATA.frame <= 0)
-  {
-      GAMEDATA.frame = 0;
-  }
-  return currentFrame;
+  return pd->graphics->getTableBitmap( GAMEDATA.bitmapTable, GAMEDATA.maze[x*y]);
+}
+
+
+void build_maze()
+{
+	for(int i=0;i<MAZE_LINEAR;i++)
+	{	
+	  GAMEDATA.maze[i] = rand() % 14;
+	}
 }
